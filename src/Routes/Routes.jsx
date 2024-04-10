4e7c17a2e9530523dade7e6aba4aa686aa5b6d65
rifat-hassan-nibir <rifathassannibir2@gmail.com>
2024-04-10 10:52:35 +0600

    Project setup done and estates and blog JSON file added

---
+++ src/Routes/Routes.jsx
@@ -0,0 +1,10 @@
+import { createBrowserRouter } from "react-router-dom";
+
+const Routes = createBrowserRouter([
+  {
+    path: "/",
+    element: <div>React Router + Tailwind + Daisy UI</div>,
+  },
+]);
+
+export default Routes;
